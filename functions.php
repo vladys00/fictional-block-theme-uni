f988207b5811c5132e9506bd5a723c8c8a4efeab
Vladys K <vldk0022@gmail.com>
2025-07-16 18:16:55 +0200

    created a class to add new blocks in a more effective way

--- functions.php
+++ functions.php
@@ -64,10 +64,30 @@ function ourLoginTitle(){
     return get_bloginfo('name');
 }
 
-function bannerBlock(){
-    wp_register_script('bannerBlockScript', get_stylesheet_directory_uri(). '/build/banner.js',array('wp-blocks', 'wp-editor'));
-    register_block_type("ourblocktheme/banner", array(
-        'editor_script' => 'bannerBlockScript',
-    ));
+// function bannerBlock(){
+//     wp_register_script('bannerBlockScript', get_stylesheet_directory_uri(). '/build/banner.js',array('wp-blocks', 'wp-editor'));
+//     register_block_type("ourblocktheme/banner", array(
+//         'editor_script' => 'bannerBlockScript',
+//     ));
+// }
+// add_action('init', 'bannerBlock');
+// THIS FROM ABOVE IS THE SAME AS BELOW, BUT IN THE CLASS FORM
+
+
+class JSXBLock {
+    function __construct($name) {
+        $this->name = $name;
+        add_action('init', [$this, 'onInit']);
+    }
+
+
+    function onInit() {
+            wp_register_script($this->name , get_stylesheet_directory_uri(). "/build/{$this->name}.js",array('wp-blocks', 'wp-editor'));
+            register_block_type("ourblocktheme/{$this->name}", array(
+            'editor_script' => $this->name,
+        ));           
+    }
 }
-add_action('init', 'bannerBlock');
+
+new JSXBLock('banner');
+new JSXBLock('genericheading');
