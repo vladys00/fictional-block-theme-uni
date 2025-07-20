8a4ad9c96c812d442b44979ded3face3e31b8049
Vladys K <vldk0022@gmail.com>
2025-07-20 09:18:07 +0200

    displaying events and blogs block

--- functions.php
+++ functions.php
@@ -73,6 +73,29 @@ function ourLoginTitle(){
 // add_action('init', 'bannerBlock');
 // THIS FROM ABOVE IS THE SAME AS BELOW, BUT IN THE CLASS FORM
 
+class PlaceholderBlock {
+    function __construct($name) {
+        $this->name = $name;
+        add_action('init', [$this, 'onInit']);
+    }
+
+    function ourRenderCallback($attributes, $content) {
+        ob_start();
+        require get_theme_file_path("/our-blocks/{$this->name}.php");
+        return ob_get_clean();
+    }
+
+    function onInit() {
+            wp_register_script($this->name , get_stylesheet_directory_uri(). "/our-blocks/{$this->name}.js",array('wp-blocks', 'wp-editor'));
+
+            register_block_type("ourblocktheme/{$this->name}", array(
+            'editor_script' => $this->name,
+            'render_callback' => [$this, 'ourRenderCallback'],
+            ));           
+    }
+}
+
+new PlaceholderBlock('eventsandblogs');
 
 class JSXBLock {
     function __construct($name, $renderCallback = null, $data = null) {
