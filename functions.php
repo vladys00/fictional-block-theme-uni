fb4f4763e8af30e594ed07b6ad8d23fc7b5d779e
Vladys K <vldk0022@gmail.com>
2025-07-23 13:49:01 +0200

    added restriction to blokc types

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@ require get_theme_file_path('/inc/search-route.php');
 require get_theme_file_path('/inc/page-banner.php');
 require get_theme_file_path('/inc/adjust-events-query.php');
 require get_theme_file_path('/inc/mapsAPI-setup.php');
+require get_theme_file_path('/inc/register-block.php');
 
 if (file_exists(__DIR__ . '/.env')) {
     $lines = file(__DIR__ . '/.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
@@ -64,87 +65,13 @@ function ourLoginTitle(){
     return get_bloginfo('name');
 }
 
-// function bannerBlock(){
-//     wp_register_script('bannerBlockScript', get_stylesheet_directory_uri(). '/build/banner.js',array('wp-blocks', 'wp-editor'));
-//     register_block_type("ourblocktheme/banner", array(
-//         'editor_script' => 'bannerBlockScript',
-//     ));
-// }
-// add_action('init', 'bannerBlock');
-// THIS FROM ABOVE IS THE SAME AS BELOW, BUT IN THE CLASS FORM
-
-class PlaceholderBlock {
-    function __construct($name) {
-        $this->name = $name;
-        add_action('init', [$this, 'onInit']);
-    }
-
-    function ourRenderCallback($attributes, $content) {
-        ob_start();
-        require get_theme_file_path("/our-blocks/{$this->name}.php");
-        return ob_get_clean();
-    }
-
-    function onInit() {
-            wp_register_script($this->name , get_stylesheet_directory_uri(). "/our-blocks/{$this->name}.js",array('wp-blocks', 'wp-editor'));
-
-            register_block_type("ourblocktheme/{$this->name}", array(
-            'editor_script' => $this->name,
-            'render_callback' => [$this, 'ourRenderCallback'],
-            ));           
-    }
+function myAllowedBlocks($allowed_block_types, $editor_context){
+    // if you are on a page or post edito screen
+    if (!empty($editor_context->post)){
+        return $allowed_block_types;
+    } 
+    // if you are on the full site editor screen
+    return array('ourblocktheme/header', 'ourblocktheme/footer', 'ourblocktheme/singlepost', 'ourblocktheme/page', 'ourblocktheme/blogindex', 'ourblocktheme/programarchive', 'ourblocktheme/singleprogram');
 }
 
-new PlaceholderBlock('eventsandblogs');
-new PlaceholderBlock('header');
-new PlaceholderBlock('footer');
-new PlaceholderBlock('singlepost');
-new PlaceholderBlock('page');
-new PlaceholderBlock('blogindex');
-new PlaceholderBlock('programarchive');
-new PlaceholderBlock('singleprogram');
-new PlaceholderBlock('singleprofessor');
-new PlaceholderBlock('archiveevent');
-new PlaceholderBlock('singleevent');
-new PlaceholderBlock('archivecampus');
-new PlaceholderBlock('singlecampus');
-
-
-class JSXBLock {
-    function __construct($name, $renderCallback = null, $data = null) {
-        $this->name = $name;
-        $this->renderCallback = $renderCallback;
-        $this->data = $data;
-        add_action('init', [$this, 'onInit']);
-    }
-
-    function ourRenderCallback($attributes, $content) {
-        ob_start();
-        require get_theme_file_path("/our-blocks/{$this->name}.php");
-        return ob_get_clean();
-    }
-
-    function onInit() {
-            wp_register_script($this->name , get_stylesheet_directory_uri(). "/build/{$this->name}.js",array('wp-blocks', 'wp-editor'));
-            
-            if ($this->data) {
-                wp_localize_script($this->name, $this->name, $this->data);
-            }
-
-            $ourArgs = array(
-            'editor_script' => $this->name,
-            );
-
-            if ($this->renderCallback) {
-                $ourArgs['render_callback'] = [$this, 'ourRenderCallback'];
-            }
-            
-            register_block_type("ourblocktheme/{$this->name}", $ourArgs);           
-    }
-}
-
-new JSXBLock('banner', true, ['fallbackimage' => get_theme_file_uri('/images/library-hero.jpg')]);
-new JSXBLock('genericheading');
-new JSXBLock('genericbutton');
-new JSXBLock('slideshow', true);
-new JSXBLock('slide', true);
+add_filter('allowed_block_types_all','myAllowedBlocks',10,2);
---
+++ inc/register-block.php
@@ -0,0 +1,88 @@
+<?php
+
+// function bannerBlock(){
+//     wp_register_script('bannerBlockScript', get_stylesheet_directory_uri(). '/build/banner.js',array('wp-blocks', 'wp-editor'));
+//     register_block_type("ourblocktheme/banner", array(
+//         'editor_script' => 'bannerBlockScript',
+//     ));
+// }
+// add_action('init', 'bannerBlock');
+// THIS FROM ABOVE IS THE SAME AS BELOW, BUT IN THE CLASS FORM
+
+class PlaceholderBlock {
+    function __construct($name) {
+        $this->name = $name;
+        add_action('init', [$this, 'onInit']);
+    }
+
+    function ourRenderCallback($attributes, $content) {
+        ob_start();
+        require get_theme_file_path("/our-blocks/{$this->name}.php");
+        return ob_get_clean();
+    }
+
+    function onInit() {
+            wp_register_script($this->name , get_stylesheet_directory_uri(). "/our-blocks/{$this->name}.js",array('wp-blocks', 'wp-editor'));
+
+            register_block_type("ourblocktheme/{$this->name}", array(
+            'editor_script' => $this->name,
+            'render_callback' => [$this, 'ourRenderCallback'],
+            ));           
+    }
+}
+
+new PlaceholderBlock('eventsandblogs');
+new PlaceholderBlock('header');
+new PlaceholderBlock('footer');
+new PlaceholderBlock('singlepost');
+new PlaceholderBlock('page');
+new PlaceholderBlock('blogindex');
+new PlaceholderBlock('programarchive');
+new PlaceholderBlock('singleprogram');
+new PlaceholderBlock('singleprofessor');
+new PlaceholderBlock('archiveevent');
+new PlaceholderBlock('singleevent');
+new PlaceholderBlock('archivecampus');
+new PlaceholderBlock('singlecampus');
+
+
+class JSXBLock {
+    function __construct($name, $renderCallback = null, $data = null) {
+        $this->name = $name;
+        $this->renderCallback = $renderCallback;
+        $this->data = $data;
+        add_action('init', [$this, 'onInit']);
+    }
+
+    function ourRenderCallback($attributes, $content) {
+        ob_start();
+        require get_theme_file_path("/our-blocks/{$this->name}.php");
+        return ob_get_clean();
+    }
+
+    function onInit() {
+            wp_register_script($this->name , get_stylesheet_directory_uri(). "/build/{$this->name}.js",array('wp-blocks', 'wp-editor'));
+            
+            if ($this->data) {
+                wp_localize_script($this->name, $this->name, $this->data);
+            }
+
+            $ourArgs = array(
+            'editor_script' => $this->name,
+            );
+
+            if ($this->renderCallback) {
+                $ourArgs['render_callback'] = [$this, 'ourRenderCallback'];
+            }
+            
+            register_block_type("ourblocktheme/{$this->name}", $ourArgs);           
+    }
+}
+
+new JSXBLock('banner', true, ['fallbackimage' => get_theme_file_uri('/images/library-hero.jpg')]);
+new JSXBLock('genericheading');
+new JSXBLock('genericbutton');
+new JSXBLock('slideshow', true);
+new JSXBLock('slide', true);
+
+?>
